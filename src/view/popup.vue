6d451404f5d8a3932c233502f6b13018a38e6b1c
kirillbowkin <kirillshishkin77@gmail.com>
2023-01-16 19:35:11 +0300

    change popup title

--- src/view/popup.vue
+++ src/view/popup.vue
@@ -1,24 +1,18 @@
 <template>
   <div class="main_app">
-    <h1>Hello {{msg}}</h1>
+    <h1>Rezka Party</h1>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'popupView',
-  data () {
-    return {
-      msg: 'popup'
-    }
-  }
-}
-
+  name: "popupView",
+};
 </script>
 
 <style>
 .main_app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
